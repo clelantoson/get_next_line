89f0e72f05eb0a67ab56d5bd8d1721f65c441df6
Camille LE LAN <cle-lan@e3r7p10.42.fr>
2021-01-25 11:43:14 +0100

    added read when we know the number of octets in advance

--- get_next_line.c
+++ get_next_line.c
@@ -15,6 +15,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUF_SIZE 4096
+
 int get_next_line(int fd, char **line)
 {
     (void)line;
@@ -25,9 +27,24 @@ int get_next_line(int fd, char **line)
 int main()
 {
     int fd;
+    int retread;
+    char buf[BUF_SIZE + 1];
 
-    fd = open("prout", O_RDONLY);
-    ft_putnbr(fd);
+    fd = open("prout", O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+    {
+        ft_putstr("open() failed\n");
+        return (1);
+    }
+    retread = read(fd, buf, BUF_SIZE);
+    buf[retread] = '\0';
+    ft_putnbr(retread);
+    ft_putstr(buf);
+    if (close(fd) == -1)
+    {
+        ft_putstr("close() failed\n");
+        return (1);
+    }
     return (0); 
 }
 
