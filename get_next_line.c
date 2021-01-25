e46f6eef18babd2ce2daba4fa9ba30398b0d5db3
Camille LE LAN <cle-lan@e3r7p10.42.fr>
2021-01-25 17:28:07 +0100

    changes

--- get_next_line.c
+++ get_next_line.c
@@ -15,38 +15,42 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUF_SIZE 4096
+#define BUF_SIZE 10
 
 int get_next_line(int fd, char **line)
 {
-    (void)line;
-    (void)fd;
+    char buf;
+    static char *str[OPEN_MAX];
+     
     return (0); 
 }
 
-int main()
-{
-    int fd;
-    int retread;
-    char buf[BUF_SIZE + 1];
+// int main()
+// {
+//     int fd;
+//     int retread;
+//     char buf[BUF_SIZE + 1];
 
-    fd = open("prout", O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-    {
-        ft_putstr("open() failed\n");
-        return (1);
-    }
-    retread = read(fd, buf, BUF_SIZE);
-    buf[retread] = '\0';
-    ft_putnbr(retread);
-    ft_putstr(buf);
-    if (close(fd) == -1)
-    {
-        ft_putstr("close() failed\n");
-        return (1);
-    }
-    return (0); 
-}
+//     fd = open("prout", O_RDONLY);
+//     if (fd == -1)
+//     {
+//         ft_putstr("open() failed\n");
+//         return (1);
+//     }
+//     while (retread = read(fd, buf, BUF_SIZE))
+//     {
+//         buf[retread] = '\0';
+//         ft_putnbr(retread);
+//         ft_putstr(buf);
+//     }
+//     ft_putnbr(retread);
+//     if (close(fd) == -1)
+//     {
+//         ft_putstr("close() failed\n");
+//         return (1);
+//     }
+//     return (0); 
+// }
 
 
 // Paramètres
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "get_next_line.h"
+int main()
+{
+    int fd;
+    char *line;
+
+    fd = open("prout", O_RDONLY);
+    while(get_next_line(fd, &line))
+    {
+        printf("line = %s\n", line);
+    }
+    free(line);
+    close(fd);
+}
